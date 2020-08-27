cd1b9c0e0c41f387f39539ea877f62dc617997f0
Euler Lobo <eulerlobo@outlook.com>
2020-08-27 09:00:07 -0300

    version 0.3.0

--- src/main/java/com/brainandeyes/tandimensions/init/ModGlobals.java
+++ src/main/java/com/brainandeyes/tandimensions/init/ModGlobals.java
@@ -4,7 +4,7 @@ public class ModGlobals {
     public static final String MODID_TAN = "toughasnails";
 
     public static final String MODID = "tandimensions";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.3.0";
     public static final String NAME = "TANDimensions";
     public static final String SERVER_PROXY = "com.brainandeyes.tandimensions.core.CommonProxy";
     public static final String CLIENT_PROXY = "com.brainandeyes.tandimensions.core.ClientProxy";
